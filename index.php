7861892604e4478bcc31b34dd5db476117615a03
Sam Hermans <sam@lumturio.com>
2017-12-05 15:26:02 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+
+// Incoming payload
+$text = $_POST['text'];
+$trigger = $_POST['trigger_word'];
+$search = ltrim(str_replace($trigger, "", $text));
+//
+//$search = $argv[1];
+
+// Do something
+$res = MrCSV::find('cronos.csv', (string)$search);
+if(sizeof($res) == 0) {
+ $message = "Zo geen cronosboite gevonden :joy:";
+} elseif(sizeof($res) == 1) {
+ $message = "Bedoel je *" . $res[0]['name'] . "* :upside_down_face: Yep dat is er eentje !";
+ $message .= "\n";
+ $message .= "> BTW: " . $res[0]['vat'] . " (" . $res[0]['type'] . ")";
+ $message .= "\n";
+ $message .= "> Adres: " . $res[0]['address'];
+ 
+} else {
+ $message = "De volgende zijn allemaal van cronos:\n";
+ foreach($res as $boite) {
+ 	$message .= " *". $boite['name'] ."*";
+ }
+}
+
+
+// Reply
+$message = ["text" => $message];
+echo json_encode($message);
+
+
+class MrCSV { 
+
+	public static function find($file, $search) {
+		$ch = fopen($file, "r");
+		$matches = [];
+		while(($row = fgetcsv($ch)) !== FALSE) {
+			if(stristr($row[0], $search)) {
+				$matches[] = array("name" => $row[0], "vat" => $row[1], "type" => $row[2], "address" => $row[3]);
+			}
+		}
+		return $matches;
+	}
+
+}
